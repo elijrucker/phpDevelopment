43ada615a5eaf2e2d68c38811f90d219ea550fa4
Eli <eli.j.rucker@gmail.com>
2025-09-14 10:44:54 -0500

    Chapter 11 exercise 1

---
+++ php_development/chapter11/postfullname.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Form for Entering Full Name</title>
+    </head>
+    <body>
+        <?php
+        // Ch11e1 - Create the self-referencing postfullname.php script
+        if (!isset($_POST['submit'])) {
+        ?>
+            <h2>Enter Full Name</h2>
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                First name:<br/>
+                <input name="first_name"/><br/>
+                Last name:<br/>
+                <input name="last_name"/><br/>
+                <p>
+                <input type="submit" name="submit" value="Submit Name" />
+            </form>
+        <?php } else { ?>
+            <h2>Greetings!</h2>
+                Hello <?= $_POST['first_name'] . " " . $_POST['last_name'] ?>
+                Thanks for submitting the form!
+        <?php } ?>
+    </body>
+</html>
