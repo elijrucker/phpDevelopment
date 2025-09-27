4c28c919fe815daa9e8369f2d57b8dcd30a6b880
elijrucker <eli.j.rucker@gmail.com>
2025-09-27 17:39:03 -0500

    Week 4 program architecture for chapter 7

---
+++ php_development/labs/week4/comparisons.php
@@ -0,0 +1,33 @@
+<!--
+    PHP Web Development with MySQL
+    Week 4 Labs - Chapter 7
+-->
+
+<html>
+    <head>
+        <title>PHP Web Development with MySQL - Week 4 / Chapter 7</title>
+    </head>
+    <body>
+        <?php
+            /*
+                Ch7e1
+                - Use '$x = random_int(1,10) to pick a number between 1 and 10
+                - Then indicate if it is between 3 and 7 inclusive
+            */
+
+
+            /*
+                Ch7e2
+                - Use '$x = random_int(1,10) to pick a number between 1 and 10
+                - Output the value of $x
+                - Indicate if it's less than 3, between 3 and 7 inclusive or 7+
+            */
+
+
+            /*
+                Ch7e3
+                - Redo Ch7e2 with compound conditional statements
+            */
+        ?>
+    </body>
+</html>
