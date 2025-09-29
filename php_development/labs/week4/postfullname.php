defbbb34bd636b1a79946f7005e8b01ab3333361
elijrucker <eli.j.rucker@gmail.com>
2025-09-29 10:21:09 -0500

    Week 4 chapter 14 excercises completed

--- php_development/labs/week4/postfullname.php
+++ php_development/labs/week4/postfullname.php
@@ -30,31 +30,54 @@
                 echo'<p class="text-danger">You forgot to enter last name</p>';
                 $output_form = true;
             }
-        }
-        else // Both first name AND last name are filled in, for entry is validated
-        {
-            // Insert full name into database
-            $dbc = mysqli_connect('localhost', 'testuser', 'testuser', 'FullName')
-                or trigger_error("Error connecting to MySQL server.", E_USER_ERROR);
-
-            $query = "INSERT INTO fullName (first_name, last_name) "
-                . "VALUES ('$first_name', '$last_name')";
-
-            $result = mysqli_query($dbc, $query)
-                or trigger_error('Error querying database.', E_USER_ERROR);
-
-            if (!$result)
+            else 
             {
-                echo("Query Error description: " . mysqli_error($dbc));
-            }
+                // Both first name AND last name are filled in, for entry is validated
+                // Insert full name into database
+                $dbc = mysqli_connect('localhost', 'testuser', 'testuser', 'FullName')
+                    or trigger_error("Error connecting to MySQL server.", E_USER_ERROR);
+
+                $query = "INSERT INTO fullName (first_name, last_name) "
+                    . "VALUES ('$first_name', '$last_name')";
+
+                $result = mysqli_query($dbc, $query)
+                    or trigger_error('Error querying database.', E_USER_ERROR);
+
+                if (!$result)
+                {
+                    echo("Query Error description: " . mysqli_error($dbc));
+             }
 
             mysqli_close($dbc);
 
-            echo "<br/><br/>Hello $first_name $last_name Thanks for submitting your full name!";
+                echo "<br/><br/>Hello $first_name $last_name Thanks for submitting your full name!";
 
-            $output_form = false;
+                $output_form = false;
+            }
         }
-        
-        ?>
+        else
+        {
+            $output_form = true;
+            $first_name = "";
+            $last_name = "";
+        }
+
+        if ($output_form): ?>
+            <h2>Enter Full Name</h2>
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                <div class="form-group">
+                    <label for="first-name">First Name</label>
+                    <input class="form-control" id="first_name" name="first_name"
+                        value="<?= $first_name ?>" placeholder="First Name">
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Last Name</label>
+                    <input class="form-control" id="last_name" name="last_name"
+                        value="<?= $last_name ?>" placeholder="Last Name">
+                </div>
+                <button type="submit" class="btn btn-primary"
+                    name="submit">Submit Name</button>
+            </form>
+        <?php endif; ?>
     </body>
 </html>
