e8dd57689b66c1e225b8019a3f676aaba6f81ac3
Eli <eli.j.rucker@gmail.com>
2025-10-07 11:54:24 -0500

    Created DB constants in dbconnection.php

---
+++ php_development/labs/week5/dbconnection.php
@@ -0,0 +1,14 @@
+<!--
+    PHP Web Development with MySQL
+    Week 5 Labs - Chapter 15
+-->
+
+<?php
+    /* dbconnection.php */
+
+    // Database connection constants
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'movieguru');
+    define('DB_PASSWORD', 'ilikemovies');
+    define('DB_NAME', 'Movie');
+?>
