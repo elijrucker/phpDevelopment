0fe1709d2d6b2ba56c7a462df3d8680b06d68e7b
Eli <eli.j.rucker@gmail.com>
2025-10-07 12:06:30 -0500

    Added DB connection and query to index.php

--- php_development/labs/week5/index.php
+++ php_development/labs/week5/index.php
@@ -9,6 +9,24 @@
         <title>Movies I Like - Week 5 Labs</title>
     </head>
     <body>
+        <div class="card">
+            <div class="card-body">
+                <h1>Movies I Like</h1>
+
+                <?php
+                require_once('dbconnection.php');
+
+                $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+                    or trigger_error('Error connecting to MySQL server for DB_NAME.', E_USER_ERROR);
+
+                $query = "SELECT id, title FROM movieListing ORDER BY title";
+
+                $result = mysqli_query($dbc, $query)
+                    or trigger_error('Error querying database movieListing', E_USER_ERROR);
+                ?>
+            </div>
+        </div>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </body>
 </html>
