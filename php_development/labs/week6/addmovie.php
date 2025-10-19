1054fb0ca2f51f1d971cd2735ec162f117a6cac6
elijrucker <eli.j.rucker@gmail.com>
2025-10-18 20:00:03 -0500

    Added movie form

--- php_development/labs/week6/addmovie.php
+++ php_development/labs/week6/addmovie.php
@@ -11,7 +11,97 @@
     <body>
         <div class="card">
             <div class="card-body">
+                <h1>Add a Movie</h1>
+                <nav>
+                    <a class="nav-link" href="index.php">Movies I Like</a>
+                </nav>
+                <hr>
+                <?php
+                    $genres = [
+                        'Action', 'Adventure', 'Comedy', 'Documentary', 'Drama',
+                        'Fantasy', 'Horror', 'Romance', 'Science Fiction',
+                        'Mystery', 'Musical', 'Western'
+                    ]
+                ?>
+                <!-- ADDITIONAL CODE HERE -->
 
+                <form class="needs-validation" novalidate method="post" 
+                action="<?= $_SERVER['PHP_SELF'] ?>">
+                    <div class="form-group row">
+                        <label for="movie_title"
+                            class="col-sm-3 col-form-label-lg">Title</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="movie_title"
+                                name="movie_title" placeholder="Title" required>
+                            <div class="invalid-feedback">
+                                Please provide a valid movie title.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="movie_rating"
+                            class="col-sm-3 col-form-label-lg">Rating</label>
+                        <div class="col-sm-8">
+                            <select class="custom-select" id="movie_rating"
+                                name="movie_rating" required>
+                                <option value="" disabled selected>Rating...</option>
+                                <option value="G">G</option>
+                                <option value="PG">PG</option>
+                                <option value="PG-13">PG-13</option>
+                                <option value="R">R</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Please select a movie rating.
+                            </div>
+                        </div>
+                    </div>
+                        <div class="form-group row">
+                            <label for="movie_director"
+                                class="col-sm-3 col-form-label-lg">Director</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="movie_director"
+                                    name="movie_director" placeholder="Director" required>
+                                <div class="invalid-feedback">
+                                    Please provide a valid movie director.
+                                </div>
+                            </div>
+                        </div>
+                    <div class="form-group row">
+                        <label for="movie_running_time_in_minutes"
+                            class="col-sm-3 col-form-label-lg">Running Time (min)</label>
+                        <div class="col-sm-8">
+                            <input type="number" class="form-control"
+                                id="movie_running_time_in_minutes"
+                                name="movie_running_time_in_minutes"
+                                placeholder="Running Time (in minutes)" required>
+                            <div class="invalid-feedback">
+                                Please provide a valid running time in minutes.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-3 col-form-label-lg">Genre</label>
+                        <div class="col-sm-8">
+                            <?php
+                                foreach ($genres as $genre)
+                                {
+                            ?>
+                                    <div class="form-check form-check-inline col-sm-3">
+                                        <input class="form-check-input" type="checkbox" 
+                                            id="movie_genre_checkbox_action"
+                                            name="movie_genre_checkbox[]"
+                                            value="<?= $genre ?>">
+                                        <label class="form-check-label" 
+                                            for="movie_genre_checkbox_action"><?= $genre ?></label>
+                                    </div>
+                            <?php
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary" type="submit"
+                        name="add_movie_submission">Add Movie</button>
+                </form>
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
