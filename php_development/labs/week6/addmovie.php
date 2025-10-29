017d8fe07ac231bfc8e100e4a08b07fba75e7843
elijrucker <eli.j.rucker@gmail.com>
2025-10-28 19:12:50 -0500

    Movies page complete

--- php_development/labs/week6/addmovie.php
+++ php_development/labs/week6/addmovie.php
@@ -106,7 +106,7 @@
 
             if ($display_add_movie_form) {
             ?>
-                <form class="needs-validation" novalidate method="post"
+                <form class="needs-validation" novalidate method="POST"
                     action="<?= $_SERVER['PHP_SELF'] ?>">
                     <div class="form-group row">
                         <label for="movie_title"
@@ -182,6 +182,27 @@
                     <button class="btn btn-primary" type="submit"
                         name="add_movie_submission">Add Movie</button>
                 </form>
+
+                <script>
+                    // JavaScript for disabling form submissions if there are invalid fields
+                    (function() {
+                        'use strict';
+                        window.addEventListener('load', function() {
+                            // Fetch all the forms we want to apply custom Bootstrap validation styles to 
+                            var forms = document.getElementsByClassName('needs-validation');
+                            // Loop over them and prevent submission
+                            var validation = Array.prototype.filter.call(forms, function(form) {
+                                form.addEventListener('submit', function(event) {
+                                    if (form.checkValidity() == false) {
+                                        event.preventDefault();
+                                        event.stopPropagation();
+                                    }
+                                    form.classList.add('was-validated');
+                                }, false);
+                            });
+                        }, false);
+                    })();
+                </script>
             <?php
             } // Display add movie form
             ?>
