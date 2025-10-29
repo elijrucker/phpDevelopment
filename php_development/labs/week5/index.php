2033046452b8aba5a6cf0bda47d635b92cd32603
elijrucker <eli.j.rucker@gmail.com>
2025-10-28 20:01:12 -0500

    Added "add movie" button to index.php

--- php_development/labs/week5/index.php
+++ php_development/labs/week5/index.php
@@ -2,57 +2,59 @@
     PHP Web Development with MySQL
     Week 5 Labs - Chapter 15
 -->
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <title>Movies I Like - Week 5 Labs</title>
-    </head>
-    <body>
-        <div class="card">
-            <div class="card-body">
-                <h1>Movies I Like</h1>
 
-                <?php
-                    require_once('dbconnection.php');
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Movies I Like - Week 5 Labs</title>
+</head>
 
-                    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-                        or trigger_error('Error connecting to MySQL server for' . DB_NAME, E_USER_ERROR);
+<body>
+    <div class="card">
+        <div class="card-body">
+            <h1>Movies I Like</h1>
+            <p>If you have a movie you would like to include, feel free to <a href='addmovie.php'> add one</a></p>
+            <?php
+            require_once('dbconnection.php');
 
-                    $query = "SELECT id, title FROM movieListing ORDER BY title";
+            $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+                or trigger_error('Error connecting to MySQL server for' . DB_NAME, E_USER_ERROR);
 
-                    $result = mysqli_query($dbc, $query)
-                        or trigger_error('Error querying database movieListing', E_USER_ERROR);
-                
-                    if (mysqli_num_rows($result) > 0):
-                ?>
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Movie Title</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+            $query = "SELECT id, title FROM movieListing ORDER BY title";
+
+            $result = mysqli_query($dbc, $query)
+                or trigger_error('Error querying database movieListing', E_USER_ERROR);
+
+            if (mysqli_num_rows($result) > 0):
+            ?>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Movie Title</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                         <?php
-                            while($row = mysqli_fetch_assoc($result))
-                            {
-                                echo "<tr><td>" .
-                                     "<a class='nav-link' href='moviedetails.php?id=" . $row['id'] .
-                                     "'>" . $row['title'] . "</a></td></tr>";
-                            }
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "<tr><td>" .
+                                "<a class='nav-link' href='moviedetails.php?id=" . $row['id'] .
+                                "'>" . $row['title'] . "</a></td></tr>";
+                        }
                         ?>
-                        </tbody>
-                    </table>
-                <?php
-                    else:
-                ?>
-                        <h3>No Movies Found :-(</h3>
-                <?php
-                    endif;
-                ?>
-            </div>
+                    </tbody>
+                </table>
+            <?php
+            else:
+            ?>
+                <h3>No Movies Found :-(</h3>
+            <?php
+            endif;
+            ?>
         </div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
 </html>
